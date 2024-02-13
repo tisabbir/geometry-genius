3879fea1d0bf0f45c7299c679b4c7142abc0cd92
TI Sabbir <tisabbir0@gmail.com>
2024-02-13 21:01:23 +0600

    the triangle is done

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,7 +1,13 @@
-function calculateTriangleArea(){
- console.log(base)   
-}
-
-const triangleBase = document.getElementById('triangle-base').value;
-const base = parseFloat(triangleBase)
-
+const btn = document.getElementById('tri-cal-btn');
+btn.addEventListener('click', function (){
+    const baseValue = document.getElementById('inputBase').value;
+    const base = parseFloat(baseValue);
+    const heightValue = document.getElementById('inputHeight').value;
+    const height = parseFloat(heightValue);
+    const triangleArea = 0.5 * base * height;
+    // console.log(triangleArea)
+    const areaCalculationContainer = document.getElementById('areaCalculationContainer');
+    const para = document.createElement('p');
+    para.innerText = 'The area of the triangle is : ' + triangleArea + ' cm^2';
+    areaCalculationContainer.appendChild(para);
+})
